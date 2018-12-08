6c9053658611c58ac26b5a2577034dd424ad50a3
otlucas <lucas.otero@despegar.com>
2018-12-08 02:48:20 -0300

    Added Spark ultra simple get that doesnt work

--- src/main/scala/ChatApplication.scala
+++ src/main/scala/ChatApplication.scala
@@ -1,3 +1,15 @@
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.io.IO
+import chatClient.ChatClientActor
+import spray.can.Http
+
+
 class ChatApplication {
+
+  implicit val system = ActorSystem()
+
+  val myListener = system.actorOf(Props[ChatClientActor], "ChatClient")
+
+  IO(Http) ! Http.Bind(myListener, interface = "localhost", port = 8080)
 
 }
