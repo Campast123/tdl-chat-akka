75d4db142a1b3766ef9e5a0b66d14e20096f7924
otlucas <lucas.otero@despegar.com>
2018-12-09 20:13:55 -0300

    Agreguè UserActor, quiero manejar los mensajes con actores

--- src/main/scala/actors/ChatActor.scala
+++ src/main/scala/actors/ChatActor.scala
@@ -4,25 +4,27 @@ import java.io.BufferedReader
 import java.util.concurrent.ConcurrentHashMap
 
 import actors.ActorChatServer.chatActor
-import akka.actor.Actor
-import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.collection.JavaConverters._
 import scala.concurrent.Future
 
 object ChatActor {
   sealed trait Event
-  final case class Join(user: User) extends Event
+  final case class Join(user: User, actorSystem: ActorSystem) extends Event
   final case class Logout(user: User) extends Event
   final case class SendMessageChat(user: User, message: String) extends Event
+  final case class ChatMessage(message: String) extends Event
   final case class CheckUsersActivity() extends Event
+  final case class doChat() extends Event
 
   class ChatActor extends Actor{
-    val users = new ConcurrentHashMap[String,User]().asScala
+    val users = new ConcurrentHashMap[String,ActorRef]().asScala
 
     def receive = {
-      case Join(user) =>
-        users += user.name -> user
+      case Join(user, actorSystem) =>
+        users += user.name -> this.createUserActor(user, actorSystem)
         this.messageAllUsers(user,"Estoy en el chat")
       case Logout(user) =>
         this.messageAllUsers(user,"Chau chau chauuuuu")
@@ -36,33 +38,23 @@ object ChatActor {
 
     def checkUsersActivity(){
       while(true){
-        for ((name, user) <- users){
-          doChat(user)
+        for ((_, user) <- users){
+          user ! doChat()
         }
         Thread.sleep(100)
       }
     }
 
-    def nonBlockingRead(in: BufferedReader) : Option[String] = {
-      if(in.ready()) Some(in.readLine()) else None
-    }
-
-    def doChat(user: User): Unit ={
-      nonBlockingRead(user.in).foreach{ input =>
-        if(input == ":quit"){
-          chatActor ! Logout(user)
-        } else {
-          chatActor ! SendMessageChat(user,input)
-        }
-      }
-    }
 
     def messageAllUsers(fromUser: User, message: String) = {
-      for((n, u) <- users){
-        u.out.println(fromUser.name+" : "+message)
+      for((_, u) <- users){
+        u ! ChatMessage(fromUser.name+" : "+message)
       }
     }
 
+    def createUserActor(user: User, actorSystem: ActorSystem): ActorRef = {
+     actorSystem.actorOf(Props(new UserActor(user.in, user.out, user.sock)),user.name)
+    }
 
   }
 
